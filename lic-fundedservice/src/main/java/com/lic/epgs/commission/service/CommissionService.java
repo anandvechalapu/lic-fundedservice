8f41bd5ce8483770a6fa952a0400772b0dcca6e0
anandvechalapu <anand.vechalapu@gmail.com>
2023-04-27 10:29:24 +0530

    Updated

--- lic-fundedservice/src/main/java/com/lic/epgs/commission/service/CommissionService.java
+++ lic-fundedservice/src/main/java/com/lic/epgs/commission/service/CommissionService.java
@@ -1,48 +1,29 @@
 package com.lic.epgs.commission.service;
 
-import com.lic.epgs.commission.dto.CommonCommissionDto;
-import com.lic.epgs.commission.dto.CommissionQuestionDetailsDto;
-import com.lic.epgs.commission.entity.CommissionQuestionDetailsTempEntity;
-import com.lic.epgs.commission.repository.CommissionRepository;
+import java.util.Date;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
-import java.util.List;
+import com.lic.epgs.commission.model.Commission;
+import com.lic.epgs.commission.repository.CommissionRepository;
 
 @Service
 public class CommissionService {
 
-    @Autowired
-    private CommissionRepository commissionRepository;
+  @Autowired
+  private CommissionRepository commissionRepository;
 
-    public CommonCommissionDto markInactive(Long id){
-        commissionRepository.markInactive(id);
-        return new CommonCommissionDto("success", "Commission question marked inactive successfully");
-    }
+  public Commission findByCommissionIdAndRole(Long commissionId, String role) {
+    return commissionRepository.findByCommissionIdAndRole(commissionId, role);
+  }
 
-    public CommonCommissionDto markActive(Long id){
-        commissionRepository.markActive(id);
-        return new CommonCommissionDto("success", "Commission question marked active successfully");
-    }
+  public void updateCommissionStatusAndWorkflowStatusByCommissionIdAndRole(Long commissionId, String role, String status, String workflowStatus) {
+    commissionRepository.updateCommissionStatusAndWorkflowStatusByCommissionIdAndRole(commissionId, role, status, workflowStatus);
+  }
 
-    public CommissionQuestionDetailsTempEntity getActive(Long id){
-        return commissionRepository.getActive(id);
-    }
-
-    public CommissionQuestionDetailsTempEntity getInactive(Long id){
-        return commissionRepository.getInactive(id);
-    }
-
-    public List<CommissionQuestionDetailsTempEntity> getAllActive(){
-        return commissionRepository.getAllActive();
-    }
-
-    public List<CommissionQuestionDetailsTempEntity> getAllInactive(){
-        return commissionRepository.getAllInactive();
-    }
-
-    public CommonCommissionDto saveCommissionQuestionDetails(List<CommissionQuestionDetailsDto> commissionQuestionDetailsDtos){
-        return commissionRepository.saveCommissionQuestionDetails(commissionQuestionDetailsDtos);
-    }
+  public void setModifiedByAndModifiedOnByCommissionIdAndRole(Long commissionId, String role, String modifiedBy, Date modifiedOn) {
+    commissionRepository.setModifiedByAndModifiedOnByCommissionIdAndRole(commissionId, role, modifiedBy, modifiedOn);
+  }
 
 }
