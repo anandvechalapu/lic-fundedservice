62376f6cc657f5cb7c26f0ea3c9e03dd7f9382bc
anandvechalapu <anand.vechalapu@gmail.com>
2023-04-27 10:30:11 +0530

    Updated

--- lic-fundedservice/src/main/java/com/lic/epgs/commission/controller/CommissionController.java
+++ lic-fundedservice/src/main/java/com/lic/epgs/commission/controller/CommissionController.java
@@ -1,53 +1,50 @@
 package com.lic.epgs.commission.controller;
 
-import com.lic.epgs.commission.dto.CommonCommissionDto;
-import com.lic.epgs.commission.dto.CommissionQuestionDetailsDto;
-import com.lic.epgs.commission.entity.CommissionQuestionDetailsTempEntity;
-import com.lic.epgs.commission.service.CommissionService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
-import java.util.List;
+import com.lic.epgs.commission.model.Commission;
+import com.lic.epgs.commission.service.CommissionQuestionMasterService;
+import com.lic.epgs.commission.service.CommissionService;
 
 @RestController
 @RequestMapping("/commission")
 public class CommissionController {
 
-    @Autowired
-    private CommissionService commissionService;
+  @Autowired
+  private CommissionService commissionService;
 
-    @PutMapping("/inactive/{id}")
-    public CommonCommissionDto markInactive(@PathVariable Long id) {
-        return commissionService.markInactive(id);
-    }
+  @Autowired
+  private CommissionQuestionMasterService commissionQuestionMasterService;
 
-    @PutMapping("/active/{id}")
-    public CommonCommissionDto markActive(@PathVariable Long id) {
-        return commissionService.markActive(id);
-    }
+  @GetMapping("/{commissionId}/{role}")
+  public ResponseEntity<Commission> getCommissionByCommissionIdAndRole(@PathVariable Long commissionId, @PathVariable String role) {
+    Commission commission = commissionService.findByCommissionIdAndRole(commissionId, role);
+    return new ResponseEntity<>(commission, HttpStatus.OK);
+  }
 
-    @GetMapping("/active/{id}")
-    public CommissionQuestionDetailsTempEntity getActive(@PathVariable Long id) {
-        return commissionService.getActive(id);
-    }
+  @PutMapping("/{commissionId}/{role}")
+  public void updateCommissionStatusAndWorkflowStatusByCommissionIdAndRole(@PathVariable Long commissionId, @PathVariable String role, String status, String workflowStatus) {
+    commissionService.updateCommissionStatusAndWorkflowStatusByCommissionIdAndRole(commissionId, role, status, workflowStatus);
+  }
 
-    @GetMapping("/inactive/{id}")
-    public CommissionQuestionDetailsTempEntity getInactive(@PathVariable Long id) {
-        return commissionService.getInactive(id);
-    }
+  @PostMapping("/{commissionId}/{role}")
+  public void setModifiedByAndModifiedOnByCommissionIdAndRole(@PathVariable Long commissionId, @PathVariable String role, String modifiedBy, Date modifiedOn) {
+    commissionService.setModifiedByAndModifiedOnByCommissionIdAndRole(commissionId, role, modifiedBy, modifiedOn);
+  }
 
-    @GetMapping("/active")
-    public List<CommissionQuestionDetailsTempEntity> getAllActive() {
-        return commissionService.getAllActive();
-    }
+  @PostMapping("/question")
+  public ResponseEntity<CommonCommissionDto> saveQuestionMaster(@RequestBody CommissionQuestionMasterDto commissionQuestionMasterDto) {
+    CommonCommissionDto commonCommissionDto = commissionQuestionMasterService.saveQuestionMaster(commissionQuestionMasterDto);
+    return new ResponseEntity<>(commonCommissionDto, HttpStatus.OK);
+  }
 
-    @GetMapping("/inactive")
-    public List<CommissionQuestionDetailsTempEntity> getAllInactive() {
-        return commissionService.getAllInactive();
-    }
-
-    @PostMapping("/save")
-    public CommonCommissionDto saveCommissionQuestionDetails(@RequestBody List<CommissionQuestionDetailsDto> commissionQuestionDetailsDtos) {
-        return commissionService.saveCommissionQuestionDetails(commissionQuestionDetailsDtos);
-    }
 }
